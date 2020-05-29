04584f5acfffe56e089fc4090b4460ee484d07b6
Brandon Williams <bwilliams.eng@gmail.com>
2020-05-28 23:02:59 -0700

    patch: add types for representing a unified diff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 //! Tools for finding and manipulating differences between files
 
 pub mod diff;
+pub mod patch;
---
+++ src/patch.rs
@@ -0,0 +1,124 @@
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Patch<'a> {
+    old: Option<&'a str>,
+    new: Option<&'a str>,
+    hunks: Vec<Hunk<'a>>,
+    eof_newline: bool,
+}
+
+impl<'a> Patch<'a> {
+    pub(crate) fn new(old: Option<&'a str>, new: Option<&'a str>, hunks: Vec<Hunk<'a>>) -> Self {
+        Self {
+            old,
+            new,
+            hunks,
+            eof_newline: true,
+        }
+    }
+}
+
+impl fmt::Display for Patch<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "--- a")?;
+        writeln!(f, "+++ b")?;
+
+        for hunk in &self.hunks {
+            write!(f, "{}", hunk)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Hunk<'a> {
+    old_range: HunkRange,
+    new_range: HunkRange,
+
+    function_context: Option<&'a str>,
+
+    lines: Vec<Line<'a>>,
+}
+
+impl<'a> Hunk<'a> {
+    pub(crate) fn new(old_range: HunkRange, new_range: HunkRange, lines: Vec<Line<'a>>) -> Self {
+        let mut old_count = 0;
+        let mut new_count = 0;
+        for line in &lines {
+            match line {
+                Line::Context(_) => {
+                    old_count += 1;
+                    new_count += 1;
+                }
+                Line::Delete(_) => old_count += 1,
+                Line::Insert(_) => new_count += 1,
+            }
+        }
+
+        assert_eq!(old_range.len, old_count);
+        assert_eq!(new_range.len, new_count);
+
+        Self {
+            old_range,
+            new_range,
+            function_context: None,
+            lines,
+        }
+    }
+}
+
+impl fmt::Display for Hunk<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "@@ -{} +{} @@", self.old_range, self.new_range)?;
+        for line in &self.lines {
+            writeln!(f, "{}", line)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct HunkRange {
+    /// The starting line number of a hunk
+    start: usize,
+    /// The hunk size (number of lines)
+    len: usize,
+}
+
+impl HunkRange {
+    pub(crate) fn new(start: usize, len: usize) -> Self {
+        Self { start, len }
+    }
+}
+
+impl fmt::Display for HunkRange {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.start)?;
+        if self.len != 1 {
+            write!(f, ",{}", self.len)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Line<'a> {
+    /// A line providing context in the diff which is present in both the old and new file
+    Context(&'a str),
+    /// A line deleted from the old file
+    Delete(&'a str),
+    /// A line inserted to the new file
+    Insert(&'a str),
+}
+
+impl fmt::Display for Line<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Line::Context(line) => write!(f, " {}", line),
+            Line::Delete(line) => write!(f, "-{}", line),
+            Line::Insert(line) => write!(f, "+{}", line),
+        }
+    }
+}
