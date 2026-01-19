6ee3ed8db290f70e9c60f4c27ae5e5cb35b2d894
Weihang Lo <me@weihanglo.tw>
2026-01-19 11:23:43 -0500

    feat(patch): new patch formatter example

---
+++ examples/patch_formatter.rs
@@ -0,0 +1,26 @@
+use diffy::create_patch;
+use diffy::PatchFormatter;
+
+fn main() {
+    let original = "first line\nlast line";
+    let modified = "first line\nmodified last line";
+
+    let patch = create_patch(original, modified);
+
+    println!("PatchFormatter::Default");
+    println!("{patch}");
+
+    let formatter = PatchFormatter::new().missing_newline_message(false);
+    println!("{formatter:?}");
+    println!("{}", formatter.fmt_patch(&patch));
+
+    let formatter = PatchFormatter::new().with_color();
+    println!("{formatter:?}");
+    println!("{}", formatter.fmt_patch(&patch));
+
+    let formatter = PatchFormatter::new()
+        .with_color()
+        .missing_newline_message(false);
+    println!("{formatter:?}");
+    println!("{}", formatter.fmt_patch(&patch));
+}
